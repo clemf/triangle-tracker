7bcd549c9ce0898cc93a3fb0396462f80b7701b0
Clem Freeman and Shimran George <clemfree@gmail.com>
2015-02-17 14:06:55 -0800

    Add print triangle sides to type lists

--- js/scripts.js
+++ js/scripts.js
@@ -9,16 +9,16 @@ var triangleTracker = function(side1, side2, side3) {
                       type: ""
   };
 
-  if( isNan(sidOne) || isNan(sideTwo) || isNan(sideThree)){
+  if( isNaN(sideOne) || isNaN(sideTwo) || isNaN(sideThree)){
     return "ERROR: PLEASE ENTER NUMBER FOR ALL THREE SIDES!";
   }
-  if(sideOne+SideTwo<SideThree || sideTwo+sideThree<sideOne || sideOne+sideThree<sideTwo){
+  if(sideOne+sideTwo<sideThree || sideTwo+sideThree<sideOne || sideOne+sideThree<sideTwo){
     return "ERROR: THESE ARE NOT VALID TRIANGLE DIMENSIONS!";
   }
   if(sideOne<=0 || sideTwo <= 0 || sideThree <= 0){
     return "ERROR: ALL SIDES MUST HAVE A VALUE GREATER THAN ZERO!";
   }
-  if(sideOne===sideTwo===sideThree){
+  if(sideOne===sideTwo && sideTwo===sideThree){
     newTriangle.type = "Equilateral";
     return newTriangle;
   }else if(sideOne===sideTwo || sideOne===sideThree || sideTwo===sideThree){
@@ -29,5 +29,24 @@ var triangleTracker = function(side1, side2, side3) {
     return newTriangle;
   }
 
-
 };
+
+$(document).ready(function() {
+  $("form#triangle-sides").submit(function(event) {
+    event.preventDefault();
+
+    var side1 = $('input#side1').val();
+    var side2 = $('input#side2').val();
+    var side3 = $('input#side3').val();
+    var triangle = triangleTracker(side1, side2, side3);
+
+    if(triangle.type=="Equilateral") {
+      $("ul#equilateral").append("<li>" + triangle.side1 + " "+ triangle.side2 + " " + triangle.side3 +  "</li>");
+    }else if(triangle.type=="Isosceles"){
+      $("ul#isosceles").append("<li>" + triangle.side1 + " "+ triangle.side2 + " " + triangle.side3 + "</li>");
+    }else {
+      $("ul#scalene").append("<li>"+ triangle.side1 + " " +triangle.side2+ " " + triangle.side3  + "</li>");
+    }
+
+  });
+});
